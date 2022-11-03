5d9bbe3e68d24a06dbc3cbcf23d06baf3c9f6676
Felipe Silva <felipe.silva@assesi.com>
2022-11-03 16:03:03 -0300

    feat: aula 02
    Signed-off-by: Felipe Silva <felipe.silva@assesi.com>

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'jdoe@example.com',
+      avatar_url: 'https://github.com/felipesilvadev.png'
+    }
+  });
+
+  const poll = await prisma.poll.create({
+    data: {
+      title: 'Example Poll',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        }
+      }
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-04T12:00:00.201Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    },
+  });
+  
+  await prisma.game.create({
+    data: {
+      date: '2022-11-05T12:00:00.201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 1,
+          secondTeamPoints: 0,
+
+          participant: {
+            connect: {
+              userId_pollId: {
+                userId: user.id,
+                pollId: poll.id
+              }
+            }
+          }
+        }
+      }
+    },
+  });
+}
+
+main();
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,126 @@
+import { FormEvent, useState } from 'react';
+import Image from 'next/image';
+
+import { api } from '../lib/axios';
+
+import logoImg from '../assets/logo.svg';
+import appPreviewImg from '../assets/app-nlw-copa-preview.png';
+import usersAvatarExampleImg from '../assets/users-avatar-example.png';
+import iconCheckImg from '../assets/icon-check.svg';
+
+interface HomeProps {
+  pollCount: number;
+  guessCount: number;
+  userCount: number;
+}
+
+export default function Home({ pollCount, guessCount, userCount }: HomeProps) {
+  const [pollTitle, setPollTitle] = useState('');
+  
+  const createPoll = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/pools', {
+        title: pollTitle,
+      });
+
+      const { code } = response.data;
+
+      await navigator.clipboard.writeText(code);
+
+      alert('Bolão criado com sucesso, o código foi copiado para a área de transferência');
+
+      setPollTitle('');
+    } catch (error) {
+      alert('Falha ao criar o bolão, tente novamente!');
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
+      <main>
+        <Image src={logoImg} alt="NLW Copa" />
+
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+{userCount}</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form onSubmit={createPoll} className="mt-10 flex items-center gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-200"
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+            value={pollTitle}
+            onChange={event => setPollTitle(event.target.value)}
+          />
+          <button
+            className="px-6 py-4 rounded bg-yellow-500 text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
+            <Image src={iconCheckImg} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{pollCount}</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          
+          <div className="w-px h-14 bg-gray-600" />
+          
+          <div className="flex items-center gap-6">
+            <Image src={iconCheckImg} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{guessCount}</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
+        quality={100}  
+      />
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {  
+  const [
+    pollCountResponse,
+    guessCountResponse,
+    usersCountResponse
+  ] = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count'),
+  ]);
+
+  return {
+    props: {
+      pollCount: pollCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: usersCountResponse.data.count
+    }
+  }
+};
